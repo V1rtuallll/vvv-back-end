1e19bf34b7cb4af86034ad48102feb40545bd58d
V1rtual <1006549138@qq.com>
2026-09-22 15:10:55 +0800

    chore: 清掉 OssUtilTest 里未使用的导入

--- src/test/java/com/v1rtual/vvv_backend/util/OssUtilTest.java
+++ src/test/java/com/v1rtual/vvv_backend/util/OssUtilTest.java
@@ -5,9 +5,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
-import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
 import org.junit.jupiter.api.Test;
